a2381d4c058937e849c26723ab5108e84ab1aaf7
Angeles <delosangelesgonzalez@gmail.com>
2026-07-16 20:44:19 +0200

    test(<Project Mapper : Null>): <Create a case to test when the api return a null object.

--- src/pods/project/project.mapper.spec.ts
+++ src/pods/project/project.mapper.spec.ts
@@ -1,1 +1,24 @@
 //test mapper
+
+import { mapProjectFromApiToVm } from "./project.mapper";
+import * as apiModel from './api/project.api-model';
+import * as viewModel from './project.vm'
+
+describe('./pods/project/project.mapper', () => {
+
+    it('It should return empty project when feeding a null value', () => {
+        // Arrange
+        const project = null;
+
+        // Act
+        const result = mapProjectFromApiToVm(project);
+
+        // Assert
+        expect(result).toEqual(viewModel.createEmptyProject());
+    });
+
+    /*----------------------*/
+
+
+
+})
