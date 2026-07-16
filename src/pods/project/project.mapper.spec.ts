e8f6abf9ee89a5ef9da3a2f2d43f35ba324f506b
Angeles <delosangelesgonzalez@gmail.com>
2026-07-16 21:08:58 +0200

    Complete all test for project mapper

--- src/pods/project/project.mapper.spec.ts
+++ src/pods/project/project.mapper.spec.ts
@@ -6,7 +6,7 @@ import * as viewModel from './project.vm'
 
 describe('./pods/project/project.mapper', () => {
 
-    it('It should return empty project when feeding a null value', () => {
+    it('Should return empty project when feeding a null value', () => {
         // Arrange
         const project = null;
 
@@ -18,6 +18,151 @@ describe('./pods/project/project.mapper', () => {
     });
 
     /*----------------------*/
+
+    it('Should return empty project when feeding a undefined value', () => {
+        // Arrange
+        const project = undefined;
+
+        // Act
+        const result = mapProjectFromApiToVm(project);
+
+        // Assert
+        expect(result).toEqual(viewModel.createEmptyProject());
+    });
+
+
+    /*----------------------*/
+
+    it('Should return expected result when feeding null employees list', () => {
+        // Arrange
+        const project: apiModel.Project = {
+            id: 'test id',
+            name: 'test name',
+            isActive: true,
+            employees: null,
+        };
+
+        const expectedResult: viewModel.Project = {
+            id: 'test id',
+            name: 'test name',
+            isActive: true,
+            employees: [],
+        };
+
+        // Act
+        const result = mapProjectFromApiToVm(project);
+
+        // Assert
+        expect(result).toEqual(expectedResult);
+    });
+
+    /*----------------------*/
+
+    it('Should return expected result when feeding undefined employees list', () => {
+        // Arrange
+        const project: apiModel.Project = {
+            id: 'test id',
+            name: 'test name',
+            isActive: true,
+            employees: undefined,
+        };
+
+        const expectedResult: viewModel.Project = {
+            id: 'test id',
+            name: 'test name',
+            isActive: true,
+            employees: [],
+        };
+
+        // Act
+        const result = mapProjectFromApiToVm(project);
+
+        // Assert
+        expect(result).toEqual(expectedResult);
+    });
+
+
+
+
+    /*----------------------*/
+
+    it('Should return expected result when feeding project with correct values and one  employee', () => {
+        // Arrange
+        const project: apiModel.Project = {
+            id: 'test id',
+            name: 'test name',
+            isActive: true,
+            employees: [{
+                id: 'test-id',
+                isAssigned: false,
+                employeeName: 'Name'
+            }],
+        };
+
+        const expectedResult: viewModel.Project = {
+            id: 'test id',
+            name: 'test name',
+            isActive: true,
+            employees: [{
+                id: 'test-id',
+                isAssigned: false,
+                employeeName: 'Name'
+            }],
+        };
+
+        // Act
+        const result = mapProjectFromApiToVm(project);
+
+        // Assert
+        expect(result).toEqual(expectedResult);
+    });
+
+
+
+
+    /*----------------------*/
+
+    it('Should return expected result when feeding project with correct values and  multiple employees', () => {
+
+        // Arrange
+        const project: apiModel.Project = {
+            id: 'test id',
+            name: 'test name',
+            isActive: true,
+            employees: [{
+                id: 'test-id',
+                isAssigned: false,
+                employeeName: 'Name'
+            }, {
+                id: 'test-id-02',
+                isAssigned: true,
+                employeeName: 'Name 02'
+            }],
+        };
+
+        const expectedResult: viewModel.Project = {
+            id: 'test id',
+            name: 'test name',
+            isActive: true,
+            employees: [{
+                id: 'test-id',
+                isAssigned: false,
+                employeeName: 'Name'
+            }, {
+                id: 'test-id-02',
+                isAssigned: true,
+                employeeName: 'Name 02'
+            }],
+        };
+
+        // Act
+        const result = mapProjectFromApiToVm(project);
+
+        // Assert
+        expect(result).toEqual(expectedResult);
+    });
+
+
 
 
 
